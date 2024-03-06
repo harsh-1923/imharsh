1d5ddff901067589753459691dcb881505f6902d
Harsh <harsh.ju.sharma@gmail.com>
2024-03-06 21:23:43 +0530

    Backend setup and vercel hosting

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  url: "mongodb+srv://harshjusharma:VYQSAQolqxLCa0jU@imharshdb.hrd1p3s.mongodb.net/?retryWrites=true&w=majority&appName=imharshdb",
+  secret: "lrNiVJKDvnxLE4gb",
+  password: "VYQSAQolqxLCa0jU",
+};
---
+++ server/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8000;
+const dburl = require("./config.js").url;
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+mongoose.connect(dburl).then(() => {
+  console.log("Connected bro");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening tp ${PORT}`);
+});
