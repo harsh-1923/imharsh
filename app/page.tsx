978322e5c39d4ebf819ecd4476bd87f16df5fa5f
Harsh <harsh.ju.sharma@gmail.com>
2025-01-04 03:39:04 +0530

    Add menu actions

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Underline from "@/components/Underline";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 items-center justify-center px-4 pb-20 gap-16 font-[family-name:var(--font-geist-sans)] pt-[80px]">
+    <main className="flex flex-1 items-center justify-center px-4 pb-20 gap-16 font-[family-name:var(--font-geist-sans)] sm:pt-[100px] pt-[80px]">
       <div className="w-full max-w-[900px]">
         <div className="flex items-center justify-center relative">
           <HeroBanner />
